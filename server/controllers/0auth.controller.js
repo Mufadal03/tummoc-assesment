c8f2730e3a8066f4fc89a599555496644d8e838d
Mufadal03 <sadrimufadal@gmail.com>
2023-05-31 17:20:13 +0530

    O auth

---
+++ server/controllers/0auth.controller.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const OauthController = express.Router();
+require('dotenv').config();
+passport.use(new GoogleStrategy(
+    {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: 'http://localhost:8000/auth/google/callback',
+    },
+
+    (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+        const user = {
+            password: profile.id,
+            username: profile.displayName,
+            email : profile.emails[0].value
+        };
+
+        done(null, user);
+    }
+)
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+OauthController.get('/', passport.authenticate('google', { scope: ['email','profile'] })
+);
+
+OauthController.get('/callback', passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        let user = req.user
+        console.log('User authenticated:', user);
+        res.cookie('user', user, {
+            httpOnly: false,
+            expires: new Date(Date.now() + 2692000000),
+            origin: 'http://localhost:3000',
+            sameSite: 'none',
+            secure: true
+        })
+        res.redirect('http://localhost:3000/home'); 
+        // res.send("ok google authenticated")
+    }
+);
+
+module.exports = OauthController; 
--- server/index.js
+++ server/index.js
@@ -3,9 +3,24 @@ const { connection } = require('./config/db')
 const { userController } = require('./controllers/user.controller')
 const { authenticator } = require('./middleware/authentication')
 const { dashboardController } = require('./controllers/dashboard.controller')
+const OauthController = require('./controllers/0auth.controller')
+const passport = require("passport");
+const session = require('express-session');
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors());
+app.use(
+    session({
+        secret: 'secret-key',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session())
+
 
 
 app.get('/', (req, res) => {
@@ -14,7 +29,7 @@ app.get('/', (req, res) => {
 
 app.use('/user',userController)
 app.use('/dashboard',authenticator, dashboardController)
-
+app.use('/auth/google',OauthController)
 
 
 app.listen(8000,async() => {
